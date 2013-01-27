0c16e025791f7453bcc9a5241872122fd48db8c0
Rafael Rivero <rafael.rivero@commit.fake>
2013-01-27 13:32:54 +0100

    Version inicial de prueba, vuelca la web de la camara

--- app.js
+++ app.js
@@ -6,7 +6,26 @@
 var express = require('express')
   , routes = require('./routes')
   , http = require('http')
-  , path = require('path');
+  , path = require('path')
+  , passport = require('passport')
+  , Strategy = require('passport-http').BasicStrategy
+  , cisco = require('./lib/cisco')
+  ;
+
+/*
+ * Estrategia "dummy" de autenticacion básica.
+ *
+ * Solicito el password para luego poder acceder a las cámaras.
+ * No compruebo el password que el usuario proporciona, simplemente
+ * lo anoto en el objeto "user".
+ */
+function fakeAuth(user, pass, done) {
+    user = pass ? { username: user, password: pass } : false;
+    process.nextTick(function() {
+        done(null, user);
+    });
+}
+passport.use(new Strategy({}, fakeAuth));
 
 var app = express();
 
@@ -18,6 +37,8 @@ app.configure(function(){
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
+  app.use(passport.initialize());
+  app.use(passport.authenticate('basic', { session: false }));
   app.use(app.router);
   app.use(express.static(path.join(__dirname, 'public')));
 });
@@ -28,7 +49,18 @@ app.configure('development', function(){
 
 app.get('/', routes.index);
 app.get('/quit', function() {
-	process.exit(0);
+    process.exit(0);
+});
+
+app.get('/cisco', function(req, res) {
+    var options = {
+        host: "10.197.108.61",
+        username: "admin",
+        password: req.user.password
+    };
+    cisco.getSID(options, function(err, sid) {
+        res.send(sid || err);
+    });
 });
 
 http.createServer(app).listen(app.get('port'), function(){
